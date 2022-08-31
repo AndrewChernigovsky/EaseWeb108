6aaeb0c0b2495936b34664007e00b5084a8c6894
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-08-31 22:31:24 +0300

    1

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,30 +1,3 @@
-// const swiperSlider = (() => {
-//   const accountantInit = new Swiper('.js-accountant-slider', {
-//     slidesPerView: 2,
-//     spaceBetween: 20,
-//     speed: 800,
-//     centerInsufficientSlides: true,
-//     pagination: {
-//       el: '.swiper-pagination',
-//       type: 'bullets',
-//       clickable: true,
-//     },
-//     breakpoints: {
-//       768: {
-//         spaceBetween: 40,
-//         slidesPerView: 3,
-//       },
-//     },
-//   });
-
-//   const init = () => {};
-
-//   return {
-//     init,
-//   };
-
-// })();
-
 const swiperMain = (() => {
   const accountantInit = new Swiper('.swiper-main', {
     slidesPerView: 1,
