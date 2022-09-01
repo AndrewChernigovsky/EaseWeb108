079dabd9242ffba5b98c7e79f992128451d3045e
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-09-01 21:05:08 +0300

    1

--- tasks/svg-sprite.js
+++ tasks/svg-sprite.js
@@ -1,30 +1,15 @@
 const gulp = require('gulp');
 const svgSprite = require('gulp-svg-sprite');
-const cheerio = require('gulp-cheerio');
-const replace = require('gulp-replace');
+const rename = require('gulp-rename');
 
 module.exports = function (options) {
   return () => {
     return gulp
       .src(`./${options.src}/icon-svg/*.svg`)
-      .pipe(
-        cheerio({
-          run: function ($) {
-            $('[style]').removeAttr('style');
-          },
-          parserOptions: { xmlMode: true },
-        })
-      )
-      .pipe(replace('&gt;', '>'))
-      .pipe(
-        svgSprite({
-          mode: {
-            symbol: {
-              sprite: '../sprite.svg',
-            },
-          },
-        })
-      )
-      .pipe(gulp.dest(`./${options.src}/images/sprite`));
+      .pipe(svgSprite({
+        inlineSvg: true
+      }))
+      .pipe(rename("sprite.svg"))
+      .pipe(gulp.dest(`./${options.dest}/images/sprite`));
   };
 };
